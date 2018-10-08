9ff33402db7760d46c00161c62c7b5af97c2e2e4
Travis Eden <daovist@gmail.com>
2018-10-08 14:13:43 -0400

    getting started

--- server/speechPassport/index.js
+++ server/speechPassport/index.js
@@ -1,6 +1,7 @@
 const passport = require('passport');
 const localLoginStrategy = require('./utils/local-login.js');
 const localSignupStrategy = require('./utils/local-signup.js');
+const existingChannelSignUpStrategy = require('./utils/existing-channel-signup');
 const serializeUser = require('./utils/serializeUser.js');
 const deserializeUser = require('./utils/deserializeUser.js');
 
@@ -8,5 +9,6 @@ passport.deserializeUser(deserializeUser);
 passport.serializeUser(serializeUser);
 passport.use('local-login', localLoginStrategy);
 passport.use('local-signup', localSignupStrategy);
+passport.use('existing-channel-signup', existingChannelSignUpStrategy);
 
 module.exports = passport;
