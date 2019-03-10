4d638fb27a33d79c11553cad547ccdd9bcefcb8b
jessop <jessopb@gmail.com>
2019-03-10 15:53:33 -0400

    tests favicon

--- server/render/renderFullPage.js
+++ server/render/renderFullPage.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const bundlePath = path.resolve('./public/bundle/bundle.js');
 const bundleHash = md5File.sync(bundlePath);
-const shortBundleHash = bundleHash.substring(0,4);
+const shortBundleHash = bundleHash.substring(0, 4);
 
 module.exports = (helmet, html, preloadedState) => {
   // take the html and preloadedState and return the full page
@@ -23,11 +23,24 @@ module.exports = (helmet, html, preloadedState) => {
             <!--google font-->
             <link href="https://fonts.googleapis.com/css?family=Roboto:300" rel="stylesheet">
             <link href="https://fonts.googleapis.com/css?family=Lora" rel="stylesheet">
+            <!-- favicons -->
+            <link rel="apple-touch-icon" sizes="180x180" href="%PUBLIC_URL%/apple-touch-icon.png">
+            <link rel="icon" type="image/png" sizes="32x32" href="%PUBLIC_URL%/favicon-32x32.png">
+            <link rel="icon" type="image/png" sizes="16x16" href="%PUBLIC_URL%/favicon-16x16.png">
+            <link rel="manifest" href="%PUBLIC_URL%/site.webmanifest">
+            <link rel="mask-icon" href="%PUBLIC_URL%/safari-pinned-tab.svg" color="#5bbad5">
+            <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico">
+            <meta name="msapplication-TileColor" content="#da532c">
+            <meta name="msapplication-config" content="%PUBLIC_URL%/browserconfig.xml">
+            <meta name="theme-color" content="#ffffff">
         </head>
         <body>
             <div id="react-app">${html}</div>
             <script>
-                window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(/</g, '\\\u003c')}
+                window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(
+                  /</g,
+                  '\\\u003c'
+                )}
             </script>
             <script src="/bundle/bundle.js?${shortBundleHash}"></script>
         </body>
